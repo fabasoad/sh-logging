4e696f78f1e918faeba056735467d8653be70dd8
fabasoad <fabasoad@gmail.com>
2024-07-13 17:34:03 +0900

    Fix issue with ignoring log level

--- lib/fabasoad-log.sh
+++ lib/fabasoad-log.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env sh
 
+FABASOAD_LOG_CONFIG_OUTPUT_FORMAT_DEFAULT="text"
 FABASOAD_LOG_CONFIG_DATE_FORMAT_DEFAULT="%Y-%m-%d %T"
 FABASOAD_LOG_CONFIG_HEADER_DEFAULT="fabasoad-log"
 FABASOAD_LOG_CONFIG_TEXT_COLOR_DEFAULT="true"
 FABASOAD_LOG_CONFIG_TEXT_FORMAT_DEFAULT="[<header>] <time> level=<level> <message>"
+FABASOAD_LOG_CONFIG_LOG_LEVEL_DEFAULT="info"
 
-_fabasoad_echo_text_color() {
+# Text modifications
+
+_fabasoad_wrap_text_with_color() {
   log_line="$1"
   level="$2"
   if [ "${FABASOAD_LOG_CONFIG_TEXT_COLOR:-${FABASOAD_LOG_CONFIG_TEXT_COLOR_DEFAULT}}" = "true" ]; then
     if [ "${level}" = "error" ]; then
-      echo "\033[91m${log_line}\033[0m"
+      printf "\033[91m${log_line}\033[0m"
     elif [ "${level}" = "warning" ]; then
-      echo "\033[93m${log_line}\033[0m"
+      printf "\033[93m${log_line}\033[0m"
     elif [  "${level}" = "info"  ]; then
-      echo "${log_line}"
+      printf "${log_line}"
     else
-      echo "\033[90m${log_line}\033[0m"
+      printf "\033[90m${log_line}\033[0m"
     fi
   else
-    echo "${log_line}"
+    printf "${log_line}"
   fi
 }
 
-_fabasoad_text_bold() {
+_fabasoad_wrap_text_with_bold() {
   if [ "${FABASOAD_LOG_CONFIG_TEXT_COLOR:-${FABASOAD_LOG_CONFIG_TEXT_COLOR_DEFAULT}}" = "true" ]; then
     echo "\033[1m$1\033[22m"
   else
@@ -31,17 +35,73 @@ _fabasoad_text_bold() {
   fi
 }
 
+# Log level
+
+_fabasoad_is_printing_error_ok() {
+  log_level="${FABASOAD_LOG_CONFIG_LOG_LEVEL:-${FABASOAD_LOG_CONFIG_LOG_LEVEL_DEFAULT}}"
+  if [ "${log_level}" != "off" ]; then
+    echo "true"
+  else
+    echo "false"
+  fi
+}
+
+_fabasoad_is_printing_warning_ok() {
+  log_level="${FABASOAD_LOG_CONFIG_LOG_LEVEL:-${FABASOAD_LOG_CONFIG_LOG_LEVEL_DEFAULT}}"
+  if [ "${log_level}" != "off" ] && [ "${log_level}" != "error" ]; then
+    echo "true"
+  else
+    echo "false"
+  fi
+}
+
+_fabasoad_is_printing_info_ok() {
+  log_level="${FABASOAD_LOG_CONFIG_LOG_LEVEL:-${FABASOAD_LOG_CONFIG_LOG_LEVEL_DEFAULT}}"
+  if [ "${log_level}" = "debug" ] || [ "${log_level}" = "info" ]; then
+    echo "true"
+  else
+    echo "false"
+  fi
+}
+
+_fabasoad_is_printing_debug_ok() {
+  log_level="${FABASOAD_LOG_CONFIG_LOG_LEVEL:-${FABASOAD_LOG_CONFIG_LOG_LEVEL_DEFAULT}}"
+  if [ "${log_level}" = "debug" ]; then
+    echo "true"
+  else
+    echo "false"
+  fi
+}
+
+# Printing
+
+_fabasoad_print_log() {
+  level="$1"
+  message="$2"
+
+  if [ "${level}" = "error" ] && [ "$(_fabasoad_is_printing_error_ok)" = "true" ]; then # error
+    printf "${message}\n" >&2
+  elif [ "${level}" = "warning" ] && [ "$(_fabasoad_is_printing_warning_ok)" = "true" ]; then # warning
+    printf "${message}\n" >&2
+  elif [ "${level}" = "info" ] && [ "$(_fabasoad_is_printing_info_ok)" = "true" ]; then # info
+    printf "${message}\n" >&2
+  elif [ "${level}" = "debug" ] && [ "$(_fabasoad_is_printing_debug_ok)" = "true" ]; then # debug
+    printf "${message}\n" >&2
+  fi
+}
+
 _fabasoad_log_text() {
   level="$1"
   message="$2"
 
-  log_line="${FABASOAD_LOG_CONFIG_TEXT_FORMAT:-${FABASOAD_LOG_CONFIG_TEXT_FORMAT_DEFAULT}}"
-  log_line=${log_line/<header>/${FABASOAD_LOG_CONFIG_HEADER:-${FABASOAD_LOG_CONFIG_HEADER_DEFAULT}}}
-  log_line=${log_line/<time>/$(date +"${FABASOAD_LOG_CONFIG_DATE_FORMAT:-${FABASOAD_LOG_CONFIG_DATE_FORMAT_DEFAULT}}")}
-  log_line=${log_line/<level>/$(_fabasoad_text_bold "${level}")}
-  log_line=${log_line/<message>/${message}}
+  text_msg="${FABASOAD_LOG_CONFIG_TEXT_FORMAT:-${FABASOAD_LOG_CONFIG_TEXT_FORMAT_DEFAULT}}"
+  text_msg=${text_msg/<header>/${FABASOAD_LOG_CONFIG_HEADER:-${FABASOAD_LOG_CONFIG_HEADER_DEFAULT}}}
+  text_msg=${text_msg/<time>/$(date +"${FABASOAD_LOG_CONFIG_DATE_FORMAT:-${FABASOAD_LOG_CONFIG_DATE_FORMAT_DEFAULT}}")}
+  text_msg=${text_msg/<level>/$(_fabasoad_wrap_text_with_bold "${level}")}
+  text_msg=${text_msg/<message>/${message}}
 
-  _fabasoad_echo_text_color "${log_line}" "${level}" >&2
+  text_msg=$(_fabasoad_wrap_text_with_color "${text_msg}" "${level}")
+  _fabasoad_print_log "${level}" "${text_msg}"
 }
 
 _fabasoad_log_json() {
@@ -55,7 +115,7 @@ _fabasoad_log_json() {
   json_msg="${json_msg}\"message\":\"${message}\""
   json_msg="${json_msg}}"
 
-  echo "${json_msg}" >&2
+  _fabasoad_print_log "${level}" "${json_msg}"
 }
 
 _fabasoad_log_xml() {
@@ -69,13 +129,15 @@ _fabasoad_log_xml() {
   xml_msg="${xml_msg}<message>${message}</message>"
   xml_msg="${xml_msg}</log>"
 
-  echo "${xml_msg}" >&2
+  _fabasoad_print_log "${level}" "${xml_msg}"
 }
 
+# Public functions
+
 fabasoad_log() {
-  level="$1"
-  message="$2"
-  config_path="$3"
+  level="${1}"
+  message="${2}"
+  config_path="${3:-""}"
 
   if [ -f "${config_path}" ]; then
     log_init_lib="$(dirname $(realpath "${0}"))/fabasoad-log-init.sh"
@@ -85,9 +147,9 @@ fabasoad_log() {
     fi
   fi
 
-  if [ "${FABASOAD_LOG_CONFIG_OUTPUT_FORMAT}" = "xml" ]; then
+  if [ "${FABASOAD_LOG_CONFIG_OUTPUT_FORMAT:-${FABASOAD_LOG_CONFIG_OUTPUT_FORMAT_DEFAULT}}" = "xml" ]; then
     _fabasoad_log_xml "${level}" "${message}"
-  elif [ "${FABASOAD_LOG_CONFIG_OUTPUT_FORMAT}" = "json" ]; then
+  elif [ "${FABASOAD_LOG_CONFIG_OUTPUT_FORMAT:-${FABASOAD_LOG_CONFIG_OUTPUT_FORMAT_DEFAULT}}" = "json" ]; then
     _fabasoad_log_json "${level}" "${message}"
   else
     _fabasoad_log_text "${level}" "${message}"
